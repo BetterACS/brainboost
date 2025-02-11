cdc7fc162a436938f53ff26c2ea877166f6afd00
jaiphattawee <jaiphattawee@gmail.com>
2025-02-11 13:43:25 +0700

    database หน้าhistory
    ทำระบบ (ยังไม่ได้เชื่อมหน้า)

---
+++ lib/screens/loading_home_wrapper.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:brainboost/router/routes.dart';
+import 'package:go_router/go_router.dart';
+
+class LoadingHomeWrapper extends StatelessWidget {
+  const LoadingHomeWrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.userChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          if (snapshot.hasData) {
+            context.go(Routes.homePage);
+          } else {
+            context.go('/login');
+          }
+        });
+
+        return const SizedBox.shrink();
+      }
+    );
+  }
+}
