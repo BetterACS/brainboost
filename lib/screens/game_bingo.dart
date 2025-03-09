94ce74cc3fa6b25bb429f70bc3f6bf235595a1a1
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2025-03-10 00:41:21 +0700

    เพิ่มฟังก์ชันเมื่อกดเเล้วเป็นคำถาม
    -สิ่งที่ติดปัญหาคือยังทำให้มันผิดเเค่ข้อเดียวไมไ่ด้
    -ui ที่ยังไม่เป๊ะ

--- lib/screens/game_bingo.dart
+++ lib/screens/game_bingo.dart
@@ -1,7 +1,137 @@
 import 'package:flutter/material.dart';
 
-class GameBingoPage extends StatelessWidget {
+class GameBingoPage extends StatefulWidget {
   const GameBingoPage({Key? key}) : super(key: key);
+
+  @override
+  _GameBingoPageState createState() => _GameBingoPageState();
+}
+
+class _GameBingoPageState extends State<GameBingoPage> {
+  List<int> numbers = [10, 25, 30, 15, 15, 25, 20, 10, 15];
+  final TextEditingController _answerController = TextEditingController();
+  bool _isAnswerCorrect = false;
+  bool _isAnswerChecked = false;
+
+  void _showQuestionDialog(int number) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor:
+              Colors.white, // Set background color of the entire dialog
+          title: Container(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            decoration: BoxDecoration(
+              color: const Color(
+                  0xFF092866), // Set background of the title to blue
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color:
+                    Colors.blue[900]!, // Optional border for the title section
+              ),
+            ),
+            child: Text(
+              "$number Points",
+              style: const TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.white, // Set the text color to white for contrast
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+
+          content: Container(
+            color: Colors.white, // Set background color of the content to white
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text(
+                  "ภายหลังสงครามโลกครั้งที่ 2 ผู้นำฝ่ายโลกคอมมิวนิสต์ขยายอิทธิพลและการแทรกแซงการปกครองในดินแดนส่วนต่างๆ ของโลกหลายแห่งข้อนี้ข้อใด",
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _answerController,
+                  decoration: InputDecoration(
+                    hintText: "Answer",
+                    hintStyle: TextStyle(color: Color(0xFFC2C2C2)),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Color(0xFF092866)),
+                    ),
+                    fillColor: _isAnswerChecked
+                        ? (_isAnswerCorrect
+                            ? Colors
+                                .green[100] // Correct answer, green background
+                            : Colors
+                                .red[100]) // Incorrect answer, red background
+                        : Colors
+                            .white, // Default background when the answer is not checked
+                    filled: true,
+                    errorText: _isAnswerChecked && !_isAnswerCorrect
+                        ? 'Incorrect Answer'
+                        : null,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () => Navigator.of(context).pop(),
+                  child: const Text("Back"),
+                ),
+                const SizedBox(width: 16),
+                Ink(
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      colors: [Color(0xFF092866), Color(0xFF205ED8)],
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      shadowColor: Colors.transparent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        // Replace with the actual correct answer
+                        _isAnswerCorrect =
+                            _answerController.text.trim() == 'correct_answer';
+                        _isAnswerChecked = true;
+                      });
+                    },
+                    child: Text(
+                      _isAnswerChecked
+                          ? _isAnswerCorrect
+                              ? 'Correct'
+                              : 'Try Again'
+                          : 'Submit',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,12 +187,11 @@ class GameBingoPage extends StatelessWidget {
               ),
               child: Column(
                 children: [
-                  // Blue rectangle above the grid (height changed to 60)
                   Container(
                     height: 60,
                     width: double.infinity,
                     decoration: BoxDecoration(
-                      color: Color(0xFF003366),
+                      color: const Color(0xFF003366),
                       borderRadius: BorderRadius.circular(8),
                     ),
                     child: const Center(
@@ -83,22 +212,24 @@ class GameBingoPage extends StatelessWidget {
                             crossAxisCount: 3,
                             crossAxisSpacing: 8,
                             mainAxisSpacing: 8,
-                            childAspectRatio: 2.5),
+                            childAspectRatio: 1),
                     itemCount: 9,
                     itemBuilder: (context, index) {
-                      List<int> numbers = [10, 25, 30, 15, 15, 25, 20, 10, 15];
-                      return Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: Colors.blue[900],
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: Text(
-                          "${numbers[index]}",
-                          style: const TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white),
+                      return GestureDetector(
+                        onTap: () => _showQuestionDialog(numbers[index]),
+                        child: Container(
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.blue[900],
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: Text(
+                            "${numbers[index]}",
+                            style: const TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
                         ),
                       );
                     },
