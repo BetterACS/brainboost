9b603951f050bf7104b0c28f4f3021b837dbdd78
naphatchanun <111549617+naphatchanun@users.noreply.github.com>
2025-04-05 20:16:07 +0700

    หน้า setting page
    -ทำ Ui AppBar
    -ยังไม่ได้เเตกsetting ย่อยๆ

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -6,18 +6,27 @@ class SettingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Setting',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        leading: const BackButton(),
-        backgroundColor: const Color(0xFFECF5FF),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1), // กำหนดความสูงของเส้น Divider
-          child: Container(
-            color: Colors.grey, // สีของเส้น Divider
-            height: 1, // ความหนาของเส้น Divider
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(90),
+        child: AppBar(
+          title: const Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: Text(
+              'Setting',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
+          leading: const Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: BackButton(),
+          ),
+          backgroundColor: const Color(0xFFECF5FF),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(1),
+            child: Container(
+              color: const Color(0xFF092866),
+              height: 1,
+            ),
           ),
         ),
       ),
